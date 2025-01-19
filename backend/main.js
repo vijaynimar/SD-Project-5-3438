4e40498af4bb24b575961bf298f54047aeed5943
vijaynimar <vijaynimar8@gmail.com>
2025-01-19 13:18:08 +0530

    cors

--- backend/main.js
+++ backend/main.js
@@ -3,11 +3,9 @@ import connection from "./db.js"
 import cors from "cors"
 import { router } from "./router/router.js"
 const app=express()
-app.use(cors({
-    origin:"*"
-}))
-app.use(express.json())
 
+app.use(express.json())
+app.use(cors())
 
 
 
