4fa1c4a15e899bb1b19f9544333072421b752e55
vijaynimar <vijaynimar8@gmail.com>
2025-01-19 13:48:15 +0530

    login

--- backend/controller/login.js
+++ backend/controller/login.js
@@ -1,26 +1,32 @@
 import { user } from "../model/user.js";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import argon2 from "argon2";
-import "dotenv/config"
-export const login =async(req,res)=>{
-    const {email,password}=req.body
-    try{
-        const User=await user.findOne({email:email})
-        if(!User){
-            return res.status(404).json({message:"user need to sign in first"})
+import "dotenv/config";
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    
+    try {  
+        const User = await user.findOne({email });
+        if (!User) {
+            return res.status(404).json({ message: "User needs to sign up first" });
         }
-        const verifies=await argon2.verify(User.password,password)
-        if(verifies){
-            if(verifies.password==password){
-                const token=jwt.sign({
-                    email:email,id:User._id
-                },process.env.jwt_key)
-                return res.status(200).json({message:"logged in sucessfully",token:token})
-              } 
-            return res.status(404).json({mesaage:"password not match"})
+        console.log(User.password);
+        const verifies = await argon2.verify(User.password, password);
+        if (verifies) {
+            const token = jwt.sign(
+                { email: email, id: User._id },
+                process.env.jwt_key,
+                { expiresIn: '1day' }
+            );
+            
+            return res.status(200).json({ message: "Logged in successfully", token: token });
         }
         
-    }catch(err){
-        return res.status(500).json({mesaage:"error in login"})
+        return res.status(401).json({ message: "Password does not match" });
+        
+    } catch (err) {
+        console.error("Error during login:", err);
+        return res.status(500).json({ message: "Internal server error", error: err.message });
     }
-}
+};
